3fd2f9901e54e8e000d5c25f0c112ccae6ba33b0
fait-arch <jdmoromenacho@gmail.com>
2025-02-18 18:48:53 -0500

    Update images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 				<Image
 					className="dark:invert"
-					src="./next.svg" // Correcto para producción y desarrollo
+					src="./next.svg"
 					alt="Next.js logo"
 					width={180}
 					height={38}
@@ -35,7 +35,7 @@ export default function Home() {
 					>
 						<Image
 							className="dark:invert"
-							src={`${basePath}/vercel.svg`}
+							src="./vercel.svg"
 							alt="Vercel logomark"
 							width={20}
 							height={20}
@@ -61,7 +61,7 @@ export default function Home() {
 				>
 					<Image
 						aria-hidden
-						src={`${basePath}/file.svg`}
+						src="./file.svg"
 						alt="File icon"
 						width={16}
 						height={16}
@@ -76,7 +76,7 @@ export default function Home() {
 				>
 					<Image
 						aria-hidden
-						src={`${basePath}/window.svg`} // Usando basePath correctamente
+						src="./window.svg" // Usando basePath correctamente
 						alt="Window icon"
 						width={16}
 						height={16}
@@ -91,7 +91,7 @@ export default function Home() {
 				>
 					<Image
 						aria-hidden
-						src={`${basePath}/globe.svg`}
+						src="./globe.svg"
 						alt="Globe icon"
 						width={16}
 						height={16}
