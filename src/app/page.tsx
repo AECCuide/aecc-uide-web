7af624eb86edd723a9936d9b39f5cc42f40d95d4
fait-arch <jdmoromenacho@gmail.com>
2025-02-18 17:33:58 -0500

    Implement basePath

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 
+
+
 export default function Home() {
+  const { basePath } = require("next/config").default().publicRuntimeConfig;
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src={`${basePath}/public/next.svg`}  // Correcto para producción y desarrollo
           alt="Next.js logo"
           width={180}
           height={38}
@@ -32,7 +36,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="@/public/vercel.svg"
+              src={`${basePath}/vercel.svg`}  
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -73,7 +77,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="@/public/window.svg"
+            src={`${basePath}/window.svg`}  // Usando basePath correctamente
             alt="Window icon"
             width={16}
             height={16}
