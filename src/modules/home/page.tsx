2dd7afbeaa14ffadd8c6c54c9ef1bcae1186a504
fait-arch <jdmoromenacho@gmail.com>
2025-03-10 03:19:54 -0500

    update color glass

--- src/modules/home/page.tsx
+++ src/modules/home/page.tsx
@@ -3,21 +3,13 @@ import { useTheme } from 'next-themes';
 import Logo from './icon/aeccIcon';
 
 export default function Home() {
-	const { theme } = useTheme(); // Obtiene el tema actual
-
-	const fillColor = theme === 'dark' ? '#ffffff' : '#000000';
-	const glasses = theme === 'dark' ? '#000000' : '#000000';
-	const glassesReflectionColor = theme === 'dark' ? '#ffffff' : '#ffffff';
 
 	return (
 		<div className="min-h-screen bg-background flex flex-col items-center justify-center p-4">
 			<div className="mb-[120px]">
 				<Logo
-					width={400}
-					height={400}
-					fillColor={fillColor}
-					glassesColor={glasses}
-					glassesReflectionColor={glassesReflectionColor}
+					width={380}
+					height={380}
 				/>
 			</div>
 		</div>
